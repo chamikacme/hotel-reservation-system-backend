aa6cbbe495384c8dc97667dfaf1b83ef72270dac
Sajana <sajanapeshitha@gmail.com>
2023-08-07 22:24:21 +0530

    Initial commit

---
+++ src/test/java/com/webdev/hotelRes/HotelResApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.hotelRes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelResApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
